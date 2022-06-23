01d98f97a5a174aa858faa309003e4fd4e282598
Ritik Tiwari <ritiktiwariwork@gmail.com>
2022-06-23 21:10:15 +0530

    Some modifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import '../styles/globals.scss';
+import LoadingBar from 'react-top-loading-bar';
 import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter;
+  const router = useRouter();
   const siteName = "Weather App";
+  const color = "#F9E84D";
 
-  return <Component {...pageProps} router={router} siteName={siteName} />
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      setProgress(70)
+    })
+    router.events.on('routeChangeComplete', () => {
+      setProgress(100)
+    })
+  }, [router]);
+
+  return <>
+    <LoadingBar
+      color={color}
+      height={5}
+      progress={progress}
+      waitingTime={800}
+      onLoaderFinished={() => setProgress(0)}
+    />
+    <Component {...pageProps} router={router} siteName={siteName} />
+  </>
 }
 
 export default MyApp
